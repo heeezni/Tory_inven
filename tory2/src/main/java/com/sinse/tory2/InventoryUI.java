ebf1d510837066f49cb7cd107997ae445b266b1c
DESKTOP-9NOGIS0\a <hjin3456@naver.com>
2025-06-18 16:51:49 +0900

    Initial commit

---
+++ tory2/src/main/java/com/sinse/tory2/InventoryUI.java
@@ -0,0 +1,10 @@
+package com.sinse.tory2;
+
+import javax.swing.JPanel;
+
+// 왼쪽 영역 UI
+public class InventoryUI extends JPanel{
+
+	
+	
+}
---
+++ tory2/src/main/java/com/sinse/tory2/MainPage.java
@@ -0,0 +1,17 @@
+package com.sinse.tory2;
+
+import javax.swing.JFrame;
+
+public class MainPage extends JFrame{
+	public MainPage() {
+		
+		
+		setSize(1920,1080);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+    public static void main( String[] args ){
+    	new MainPage();
+    }
+}
